aa19039f37a81dc3474d7b88f5b0d27b1708b4e7
julioch <juliojch89@gmail.com>
2026-02-24 18:48:43 -0300

    Estructura inicial limpia

---
+++ index.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+print("¡Listo! TensorFlow versión:", tf.__version__)
+print("Iniciando el nuevo proyecto de IA para Julio...")
+# Aquí irá el código de tu modelo más adelante
